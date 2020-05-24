2c76ba2b34a02d39d870b88e064c2766518af32e
Kirill Enykeev <enykeev@mech.sh>
2020-05-24 18:37:57 +0700

    consolidate migrations

--- components/controller/migrations/20200411192416_initial.js
+++ components/controller/migrations/20200411192416_initial.js
@@ -6,10 +6,18 @@ exports.up = (knex) => {
       t.string('triggered_by')
       t.string('action')
       t.jsonb('parameters')
+      t.timestamps(false, true)
+      t.enum('status', ['requested', 'claimed', 'running', 'completed'])
     })
     .createTable('results', t => {
       t.string('id').primary()
       t.jsonb('result')
+      t.enum('status', ['succeeded', 'failed', 'timeout'])
+    })
+    .createTable('rules', t => {
+      t.string('id').primary()
+      t.text('code')
+      t.timestamps(false, true)
     })
 }
 
@@ -17,4 +25,5 @@ exports.down = (knex) => {
   return knex.schema
     .dropTable('executions')
     .dropTable('results')
+    .dropTable('rules')
 }
