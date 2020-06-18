f269873d37ba4b2bc8f241b06955b54091453380
Kirill Enykeev <enykeev@mech.sh>
2020-06-18 10:52:07 +0700

    reintroduce migration service in docker-compose

--- deployments/default/knexfile.js
+++ deployments/default/knexfile.js
@@ -25,7 +25,11 @@ module.exports = {
     client: 'pg',
     connection: process.env.PG_CONNECTION_STRING,
     migrations: {
+      directory: path.join(__dirname, '../../components/core/migrations'),
       tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: path.join(__dirname, 'seeds')
     }
   }
 }
