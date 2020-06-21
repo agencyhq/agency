15bd59de68368b78b994c87986f416b385b670eb
Kirill Enykeev <enykeev@mech.sh>
2020-06-21 11:50:20 +0700

    update reminders with basic day schedule

--- deployments/personal/rules/telegram_echo.js
+++ deployments/personal/rules/telegram_echo.js
@@ -1,14 +1,36 @@
 const chatId = 127792927
 
+const legionInvasion = new Date('2020-06-16T01:30:00Z')
+
 const reminders = [{
-  time: ({ hours, minutes }) => hours === 14 && minutes === 0,
-  text: "2 o'clock"
+  time: '09:00',
+  text: 'Wake up, sunshine!'
 }, {
-  time: '14:30',
-  text: 'passed'
+  time: '10:00',
+  text: 'Breakfast time'
 }, {
-  time: '14:30',
-  text: 'passed again'
+  time: '13:00',
+  text: 'Lunch time'
+}, {
+  time: '16:00',
+  text: 'Tea time'
+}, {
+  time: '20:00',
+  text: 'Dinner time'
+}, {
+  time: '22:00',
+  text: "Let's start winding down"
+}, {
+  time: '00:00',
+  text: 'Time to go to sleep'
+}, {
+  time: ({ iso }) => {
+    const minutesSinceInvasion = (new Date(iso) - legionInvasion) / (60 * 1000)
+    const invasionPeriod = 18 * 60 + 30
+
+    return !(minutesSinceInvasion % invasionPeriod)
+  },
+  text: 'invasion has started'
 }]
 
 function match (event, reminder) {
