8d560202779d201cbc85a790c1c63939d5068cd7
Kirill Enykeev <enykeev@mech.sh>
2020-06-25 12:47:44 +0700

    add cron sensor tests

--- components/sensor-cron/src/sensor.js
+++ components/sensor-cron/src/sensor.js
@@ -11,17 +11,38 @@ const rpc = new RPC.Client(process.env.AGENCY_URL || 'ws://localhost:3000/')
 
 metrics.instrumentRPCClient(rpc)
 
-const {
-  PORT = 3001,
-  METRICS = false,
-  AGENCY_TOKEN
-} = process.env
+async function handleTick () {
+  const date = new Date()
 
-if (METRICS) {
-  metrics.createServer(PORT)
+  date.setMilliseconds(0)
+
+  const trigger = {
+    id: crypto.randomBytes(16).toString('hex'),
+    type: 'cron',
+    event: {
+      iso: date.toISOString(),
+      date: date.getDate(),
+      month: date.getMonth(),
+      year: date.getFullYear(),
+      hours: date.getHours(),
+      minutes: date.getMinutes()
+    }
+  }
+
+  await rpc.call('trigger.emit', trigger, { become: '*' })
 }
 
 async function main () {
+  const {
+    PORT = 3001,
+    METRICS = false,
+    AGENCY_TOKEN
+  } = process.env
+
+  if (METRICS) {
+    metrics.createServer(PORT)
+  }
+
   rpc.on('disconnected', () => {
     process.exit(1)
   })
@@ -30,28 +51,11 @@ async function main () {
   await rpc.auth({ token: AGENCY_TOKEN })
   await rpc.notify('ready')
 
-  cron.schedule('* * * * *', async () => {
-    const date = new Date()
-
-    date.setMilliseconds(0)
-
-    const trigger = {
-      id: crypto.randomBytes(16).toString('hex'),
-      type: 'cron',
-      event: {
-        iso: date.toISOString(),
-        date: date.getDate(),
-        month: date.getMonth(),
-        year: date.getFullYear(),
-        hours: date.getHours(),
-        minutes: date.getMinutes()
-      }
-    }
-
-    await rpc.call('trigger.emit', trigger, { become: '*' })
-  })
+  cron.schedule('* * * * *', handleTick)
 }
 
 module.exports = {
-  main
+  main,
+  rpc,
+  handleTick
 }
--- components/sensor-cron/test/test_sensor.js
+++ components/sensor-cron/test/test_sensor.js
@@ -1,13 +1,95 @@
+const metrics = require('@agencyhq/agency-metrics')
+const cron = require('node-cron')
+
 const chai = require('chai')
+const sinon = require('sinon')
+
+const {
+  main: sensor,
+  rpc,
+  handleTick
+} = require('../src/sensor')
 
 const { expect } = chai
 chai.use(require('chai-as-promised'))
 chai.use(require('sinon-chai'))
 
+const sandbox = sinon.createSandbox()
+
 describe('Cron Sensor', () => {
-  describe('#/http', () => {
-    it.skip('should notify api of new trigger', () => {
-      expect(1).to.be.equal(1)
+  afterEach(() => {
+    delete process.env.METRICS
+    delete process.env.PORT
+    rpc.removeAllListeners()
+    sandbox.restore()
+  })
+
+  it('should start metrics server if env variable is set', async () => {
+    sandbox.stub(cron, 'schedule').callsFake()
+    sandbox.stub(rpc, 'connect').callsFake()
+    sandbox.stub(rpc, 'auth').returns({})
+    sandbox.stub(rpc, 'notify').callsFake()
+
+    sandbox.stub(metrics, 'createServer').callsFake()
+
+    process.env.METRICS = '1'
+    process.env.PORT = '0000'
+
+    await sensor()
+
+    expect(metrics.createServer).to.be.calledOnceWith('0000')
+  })
+
+  it('should connect to the API', async () => {
+    sandbox.stub(cron, 'schedule').callsFake()
+    sandbox.stub(rpc, 'connect').callsFake()
+    sandbox.stub(rpc, 'auth').returns({})
+    sandbox.stub(rpc, 'notify').callsFake()
+
+    process.env.AGENCY_TOKEN = 'faketoken'
+
+    await sensor()
+
+    expect(rpc.connect).to.be.calledOnce
+    expect(rpc.auth).to.be.calledOnceWith({ token: 'faketoken' })
+    expect(cron.schedule).to.be.calledOnceWith('* * * * *', handleTick)
+    expect(rpc.notify).to.be.calledOnceWith('ready')
+  })
+
+  it('should exit on disconnect from RPC', async () => {
+    sandbox.stub(cron, 'schedule').callsFake()
+    sandbox.stub(rpc, 'connect').callsFake()
+    sandbox.stub(rpc, 'auth').returns({})
+    sandbox.stub(rpc, 'notify').callsFake()
+
+    sandbox.stub(process, 'exit').callsFake()
+
+    await sensor()
+
+    rpc.emit('disconnected')
+
+    expect(process.exit).to.be.calledOnce
+  })
+
+  describe('#handleTick()', () => {
+    it('should emit a trigger', async () => {
+      sandbox.stub(rpc, 'call').callsFake()
+
+      await handleTick()
+
+      const eventShape = sinon.match({
+        id: sinon.match.string,
+        type: 'cron',
+        event: sinon.match({
+          date: sinon.match.number,
+          month: sinon.match.number,
+          year: sinon.match.number,
+          hours: sinon.match.number,
+          minutes: sinon.match.number,
+          iso: sinon.match.string
+        })
+      })
+      expect(rpc.call).to.be.calledOnceWith('trigger.emit', eventShape, { become: '*' })
     })
   })
 })
