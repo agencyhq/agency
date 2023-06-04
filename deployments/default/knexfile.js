71c14b238f6451fcc853ff5b21610cd53162a650
Kirill Enykeev <enykeev@mech.sh>
2023-06-04 14:38:17 +0200

    remove esm flag

--- deployments/default/knexfile.js
+++ deployments/default/knexfile.js
@@ -1,34 +1,35 @@
 import path from 'path'
+import * as url from 'url'
 
-export default {
-  development: {
-    client: 'pg',
-    connection: {
-      user: 'ifttt',
-      password: 'mypassword'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: path.join(__dirname, '../../components/core/migrations'),
-      tableName: 'knex_migrations'
-    },
-    seeds: {
-      directory: path.join(__dirname, 'seeds')
-    }
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+
+export const development = {
+  client: 'pg',
+  connection: {
+    user: 'ifttt',
+    password: 'mypassword'
   },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    directory: path.join(__dirname, '../../components/core/migrations'),
+    tableName: 'knex_migrations'
+  },
+  seeds: {
+    directory: path.join(__dirname, 'seeds')
+  }
+}
 
-  staging: {
-    client: 'pg',
-    connection: process.env.PG_CONNECTION_STRING,
-    migrations: {
-      directory: path.join(__dirname, '../../components/core/migrations'),
-      tableName: 'knex_migrations'
-    },
-    seeds: {
-      directory: path.join(__dirname, 'seeds')
-    }
+export const staging = {
+  client: 'pg',
+  connection: process.env.PG_CONNECTION_STRING,
+  migrations: {
+    directory: path.join(__dirname, '../../components/core/migrations'),
+    tableName: 'knex_migrations'
+  },
+  seeds: {
+    directory: path.join(__dirname, 'seeds')
   }
 }
